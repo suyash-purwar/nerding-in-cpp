fea674fd59cfd53926075a31741116c83a920d1d
suyash-purwar <purwarsuyash06@gmail.com>
2022-05-20 05:27:13 +0530

    Push basics2, practicing lists

--- code/basics/basics2.cpp
+++ code/basics/basics2.cpp
@@ -2,10 +2,18 @@
 
 using namespace std;
 
-// Empty
+// Array
 
 int main() {
-    string name = "Suyash";
-    cout << name;
+    // Size of the array has to be defined during initialization of arrays
+    float savings[10] = { 100, 232, 83, 278, 387, 243, 190 };
+    savings[7] = 182;
+
+    int arr[3];
+    cout << arr[1] << endl; // Return either 0 or garbage value
+
+    cout << savings[7] << endl; // Return 182
+    cout << savings[8] << endl; // Returns either 0 or garbage value
+    cout << savings[11]; // Return non-sensical value. Maybe garbage value
     return 0;
 }
