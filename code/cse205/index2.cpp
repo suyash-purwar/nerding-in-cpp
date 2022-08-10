5ec871f4c6ee6c736a50502c9aabc1fa16eb644b
suyash-purwar <purwarsuyash06@gmail.com>
2022-08-10 09:35:11 +0530

    Push linerS

--- code/cse205/index2.cpp
+++ code/cse205/index2.cpp
@@ -6,7 +6,9 @@ using std::endl;
 
 class Array {
     public:
+        int maxsize = 10;
         int x[10];
+        
         Array() {
 
         }
@@ -34,12 +36,64 @@ class Array {
             }
             cout << endl;
         }
+
+        bool linearSearch() {
+            int element;
+            cout << "Enter the element to search: ";
+            cin >> element;
+            for (int i = 0; i > 10; i++) {
+                if (x[i] == element) {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        // [12, 34, 2, 12, 24, 0, 0, 23, 4, 0] 
+        // Entering at x(3rd index) index
+        // Move the elements from x
+        // Insert the element at x
+        void insertElement() {
+            int element;
+            int position;
+            cout << "Enter the element to insert: ";
+            cin >> element;
+            cout << "\nEnter the index at which to insert the element: ";
+            cin >> position;
+
+            if (position > maxsize-1) {
+                cout << "Insertion not possible!" << endl;
+            } else {
+                for (int i = maxsize; position < maxsize; maxsize--) {
+                    x[maxsize] = x[maxsize-1];
+                }
+                x[position] = element;
+                cout << "Insertion successful!" << endl;
+            }
+        }
+
+        void deleteElement() {
+            int index;
+            cout << "Enter the index at which to delete the element: ";
+            cin >> index;
+            if (index > maxsize-1) {
+                cout << "Deletion not possible!" << endl;
+            } else {
+                for (int i = index; maxsize > index; index++) {
+                    x[index] = x[index+1];
+                }
+                cout << "Deletion successful!" << endl;
+            }
+        }
 };
 
 int main() {
     Array arr1;
     arr1.readElement();
     arr1.displayElementForward();
-    arr1.displayElementBackward();
+    // arr1.displayElementBackward();
+    // arr1.insertElement();
+    arr1.deleteElement();
+    arr1.displayElementForward();
     return 0;
 }
