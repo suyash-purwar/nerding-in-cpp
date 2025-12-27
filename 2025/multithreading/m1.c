ce39a8517e51f8bd46c50c84c7853b5b4ca82f3e
suyash-purwar <purwarsuyash06@gmail.com>
2025-12-28 00:26:12 +0530

    practice basics of multithreading in c

--- 2025/multithreading/m1.c
+++ 2025/multithreading/m1.c
@@ -2,21 +2,36 @@
 #include <pthread.h>
 #include <unistd.h>
 
-void *worker()
+void* worker1()
 {
-    printf("Hello, World! from Thread\n");
+    printf("Thread1 worker\n");
     sleep(2);
     return NULL;
 }
 
+void* worker2(void* arg) {
+    char* str = (char*)arg;
+
+    while (1) {
+        printf("%s\n", str);
+        sleep(1);
+    }
+}
+
 int main()
 {
-    pthread_t tid;
+    pthread_t tid1;
+    pthread_t tid2;
 
-    pthread_create(&tid, NULL, worker, NULL);
-    pthread_join(tid, NULL);
+    pthread_create(&tid1, NULL, worker1, NULL);
+    pthread_join(tid1, NULL);
 
-    printf("Hello, World! from Main\n");
+    static char* string = "Thread2 worker";
+    pthread_create(&tid2, NULL, worker2, (void*)string);
+
+    printf("Main thread\n");
+
+    sleep(10);
 
     return 0;
 }
